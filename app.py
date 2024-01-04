88b0a52c5c3ce560672cd6917190a47fd905308b
Yoda-TinyML <148352250+Yoda-TinyML@users.noreply.github.com>
2024-01-04 15:08:20 +0500

    Update app.py add quantization

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ from peft import PeftModel, PeftConfig
 from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+import torch
 
 class InferlessPythonModel:
     def initialize(self):
-        quantization_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16)
+        quantization_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16,bnb_4bit_quant_type="nf4")
         #adapter_file = "G-ML-Hyly/stg-cli13b-t6-cdp-ca.mt.him.cln.inter-b4s1e1-20231220-1052"
         config = PeftConfig.from_pretrained("G-ML-Hyly/stg-cli13b-t6-cdp-ca.mt.him.cln.inter-b4s1e1-20231219-1611")
         #config = PeftConfig.from_pretrained(adapter_file)
-        self.model = AutoModelForCausalLM.from_pretrained("codellama/CodeLlama-13b-Instruct-hf")
-        self.model = PeftModel.from_pretrained(self.model, "G-ML-Hyly/stg-cli13b-t6-cdp-ca.mt.him.cln.inter-b4s1e1-20231219-1611",quantization_config=quantization_config)
+        self.model = AutoModelForCausalLM.from_pretrained("codellama/CodeLlama-13b-Instruct-hf",return_dict=True,quantization_config=quantization_config,device_map={"": 0},trust_remote_code=True,)
+        self.model = PeftModel.from_pretrained(self.model, "G-ML-Hyly/stg-cli13b-t6-cdp-ca.mt.him.cln.inter-b4s1e1-20231219-1611")
         #self.model = PeftModel.from_pretrained(self.model, adapter_file)
         self.model.to("cuda:0")
         self.tokenizer = AutoTokenizer.from_pretrained("codellama/CodeLlama-13b-Instruct-hf")
